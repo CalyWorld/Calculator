51464aa03edc0e60cdb39432d50a1b517eb2fd7f
CalAnizoba <obinnaanizoba5@gmail.com>
2022-08-01 12:36:42 +0800

    fixed bugs

--- calculator.js
+++ calculator.js
@@ -12,152 +12,145 @@ let currentNumber = "";
 let displayValue = "";
 let operatorName = "";
 
-function add(num1, num2){
-    return num1 + num2;
-  }
-  function subtract(num1, num2){
-    return num1 - num2;
-  }
-  function multiply(num1, num2){
-    return num1 * num2;
-  }
-  function divide(num1, num2){
-    return num1 / num2;
-  }
-  // function percentage(num1, num2){
-     
-  //     num1 = 100;
-  //     return num1/num1;
-  // }
-  // function plusMinus(num1, num2){
-  //   return -num2 
-  // }
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
 
-  function operate(operator, num1, num2){
-    switch(operator){
-      case '+' :
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case '+':
       display.textContent = add(num1, num2);
       break;
-      case '-':
-        display.textContent =  subtract(num1, num2);
-        break;
-      case '*':
-        display.textContent =  multiply(num1, num2);
-       break;
-      case '÷':
-        display.textContent = divide(num1, num2);
-        break;
+    case '-':
+      display.textContent = subtract(num1, num2);
+      break;
+    case '*':
+      display.textContent = multiply(num1, num2);
+      break;
+    case '÷':
+      display.textContent = divide(num1, num2);
+      break;
 
-        // default:
-        //         console.log('invalid operator');
-        //   break;
-    }
-    currentNumber = display.textContent;   //stores calculated value
-    operatorName = ""; //operator variable is cleared for next operation
-    displayValue = ""; //displayValue is cleared for next operation
-   
+    // default:
+    //         console.log('invalid operator');
+    //   break;
   }
+  currentNumber = display.textContent;   //stores calculated value
+  operatorName = ""; //operator variable is cleared for next operation
+  displayValue = ""; //displayValue is cleared for next operation
 
-  
-  
-  
-  number.forEach((itemNumber)=>{
-    //populate display
-    itemNumber.addEventListener('click', (e)=>{  
-     
-      //if displayValue is empty, number clicked equals displayValue
-      if(!displayValue){
-        displayValue = e.currentTarget.textContent;
-        
-        //else append the numbers clicked into displayValue
-      }else{
-        displayValue += e.currentTarget.textContent;
-      }
-      //replaces the display with numbers clicked 
-      display.textContent = displayValue;
-       
- 
-    });
+}
+
+
+
+
+number.forEach((itemNumber) => {
+  //populate display
+  itemNumber.addEventListener('click', (e) => {
+
+    //if displayValue is empty, number clicked equals displayValue
+    if (!displayValue) {
+      displayValue = e.currentTarget.textContent;
+
+      //else append the numbers clicked into displayValue
+    } else {
+      displayValue += e.currentTarget.textContent;
+    }
+    //replaces the display with numbers clicked 
+    display.textContent = displayValue;
+
+
   });
-                          
-    
-  operators.forEach((itemOperator)=>{
-    itemOperator.addEventListener('click', (e)=>{
-            //currentNUmber and operator are stored then calculate(); function is being called upon 
-           
+});
 
-            if(currentNumber && operatorName){
-              operate(operatorName, Number(currentNumber), Number(displayValue)); 
-            }
-            //if operator is not seleted but currentNumber and displayValue are stored, equate each number and clear displayValue
-            else if(!operatorName && currentNumber && displayValue){
-                  currentNumber = displayValue;
-                  displayValue = "";
-            }
-            //else if currentNumber exists from previous calculation, reset displayValue
-            else if(currentNumber){
-              displayValue = "";
-            }
-            //else if no values are stored, store first number in currentNumber and reset displayValue
-            else{
-              currentNumber = Number(displayValue);
-              displayValue = "";
-            }
-            //operator.textContent clicked is stored in variable operatorName
-            operatorName += e.currentTarget.textContent;
-          
-    });
+
+operators.forEach((itemOperator) => {
+  itemOperator.addEventListener('click', (e) => {
+    //currentNUmber and operator are stored then calculate(); function is being called upon 
+
+
+    if (currentNumber && operatorName) {
+      operate(operatorName, Number(currentNumber), Number(displayValue));
+    }
+    //if operator is not seleted but currentNumber and displayValue are stored, equate each number and clear displayValue
+    else if (!operatorName && currentNumber && displayValue) {
+      currentNumber = displayValue;
+      displayValue = "";
+    }
+    //else if currentNumber exists from previous calculation, reset displayValue
+    else if (currentNumber) {
+      displayValue = "";
+    }
+    //else if no values are stored, store first number in currentNumber and reset displayValue
+    else {
+      currentNumber = Number(displayValue);
+      displayValue = "";
+    }
+    //operator.textContent clicked is stored in variable operatorName
+    operatorName += e.currentTarget.textContent;
+
   });
+});
 
 
 
-  equal.addEventListener('click', ()=>{
-      operate(operatorName, Number(currentNumber), Number(displayValue)); 
-   
-  });
+equal.addEventListener('click', () => {
+  operate(operatorName, Number(currentNumber), Number(displayValue));
+
+});
 
 
-  
-  clear.addEventListener('click', ()=>{
-    display.textContent = 0; //stes the textContent to 0
-        displayValue = ""; //resets displayValue
-        operatorName = ""; //resets operatorName
-        currentNumber = ""; //restes currentNUmber
-      });
-    
 
-      percentage.addEventListener('click', ()=>{
-          if(displayValue){
-              displayValue = Number(displayValue/100);
-              display.textContent = displayValue;
-          }else{
-            currentNumber = Number(currentNumber/100);
-            display.textContent = currentNumber;
-          }
-      });
+clear.addEventListener('click', () => {
+  display.textContent = 0; //sets the textContent to 0
+  displayValue = ""; //resets displayValue
+  operatorName = ""; //resets operatorName
+  currentNumber = ""; //restes currentNUmber
+});
 
-      plusEqual.addEventListener('click', ()=>{
-        //if displayValue
-        if(displayValue){
-          displayValue = Number(displayValue)*-1;
-          display.textContent = displayValue;
-          //else currentNumber
-         }else{
-          currentNumber = Number(currentNumber)*-1;
-          display.textContent = currentNumber;
-         }
-      });
 
-      dot.addEventListener('click', ()=>{
-        if (displayValue.toString().indexOf('.') === -1) {
-          //if there is no displayValue, add dot
-          if (!displayValue){
-              displayValue = ".";
-          //else if there is add it to displayValue
-          } else {
-              displayValue += ".";
-          }
-          //add displayValue to display
-          display.textContent = displayValue;
-      }
-      });
+percentage.addEventListener('click', () => {
+  if (displayValue) {
+    displayValue = Number(displayValue / 100);
+    display.textContent = displayValue;
+  } else {
+    currentNumber = Number(currentNumber / 100);
+    display.textContent = currentNumber;
+  }
+});
+
+plusEqual.addEventListener('click', () => {
+  //if displayValue
+  if (displayValue) {
+    displayValue = Number(displayValue) * -1;
+    display.textContent = displayValue;
+    //else currentNumber
+  } else {
+    currentNumber = Number(currentNumber) * -1;
+    display.textContent = currentNumber;
+  }
+});
+
+dot.addEventListener('click', () => {
+  if (displayValue.toString().indexOf('.') === -1) {
+    //if there is no displayValue, add dot
+    if (!displayValue) {
+      displayValue = ".";
+      //else if there is add it to displayValue
+    } else {
+      displayValue += ".";
+    }
+    //add displayValue to display
+    display.textContent = displayValue;
+  }
+});
